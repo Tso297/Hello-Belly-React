e6896412ebb50f02db426b81fc09c368788917d7
Travis <you@example.com>
2024-05-15 23:47:59 -0400

    Remove firebase-config.ts from tracking

--- src/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAcIGJIooN-pJfcrfxvidhKUudjHM6swZo",
-  authDomain: "hello-belly-22577.firebaseapp.com",
-  projectId: "hello-belly-22577",
-  storageBucket: "hello-belly-22577.appspot.com",
-  messagingSenderId: "100815937010",
-  appId: "1:100815937010:web:d39a6ec1c26d72c1ca5f6b",
-  measurementId: "G-SD13L73WWQ"
-};
